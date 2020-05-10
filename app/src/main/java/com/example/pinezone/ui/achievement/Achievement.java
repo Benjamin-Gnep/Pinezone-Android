d6f519a765ff283fe4eebcb50a9eb3793b46d37e
Benjamin-Gnep <56106993+Benjamin-Gnep@users.noreply.github.com>
2020-05-10 18:31:40 +0800

    添加 页面活动

---
+++ app/src/main/java/com/example/pinezone/ui/achievement/Achievement.java
@@ -0,0 +1,16 @@
+package com.example.pinezone.ui.achievement;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pinezone.R;
+
+public class Achievement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_achievement);
+    }
+}
---
+++ app/src/main/java/com/example/pinezone/ui/publish/PublishType.java
@@ -0,0 +1,16 @@
+package com.example.pinezone.ui.publish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pinezone.R;
+
+public class PublishType extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publish_type);
+    }
+}
