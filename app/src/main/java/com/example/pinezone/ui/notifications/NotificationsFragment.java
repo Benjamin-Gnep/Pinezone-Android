94222a5dfcc3556df4bbf60772a7d895e085a10f
Benjamin-Gnep <56106993+Benjamin-Gnep@users.noreply.github.com>
2020-05-31 10:20:25 +0800

    添加 消息页面布局注释

--- app/src/main/java/com/example/pinezone/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/pinezone/ui/notifications/NotificationsFragment.java
@@ -1,6 +1,7 @@
 package com.example.pinezone.ui.notifications;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
@@ -16,14 +18,21 @@ import com.example.pinezone.R;
 
 public class NotificationsFragment extends Fragment {
 
+    private static final String TAG = "NotificationsFragment";
     private NotificationsViewModel notificationsViewModel;
+    private View view;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        notificationsViewModel =
-                ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
-
-        return root;
+        view = inflater.inflate(R.layout.fragment_notifications, container, false);
+        ConstraintLayout actionBarView = view.findViewById(R.id.action_bar);
+        if(actionBarView == null){
+            Log.e(TAG, "no exist");
+        }
+        TextView textView = (TextView) actionBarView.findViewById(R.id.nobutton_bar_title);
+        textView.setText("我的消息");
+        return view;
     }
+
+
 }
